b1fc2244427d8757f1cc4625a942ad1c85731ecd
unknown <jinjihoon@gmail.com>
2020-10-13 22:42:34 +0900

    [jin] 휴대폰 본인 인증 완료(네이버 SENS API 활용)

--- routes/auth.js
+++ routes/auth.js
@@ -1,34 +1,81 @@
 require('dotenv').config();
 const app = require('express').Router();
 const Cache = require('memory-cache');
-const accountSid = process.env.ACCOUNTSID;
-const authToken = process.env.AUTHTOKEN;
-const client = require('twilio')(accountSid, authToken);
+const request = require('request');
+const CryptoJS = require('crypto-js');
 
-app.post('/', (req, res) => {
+// 환경변수 설정
+const NCP_serviceID = process.env.SERVICEID;
+const NCP_accessKey = process.env.ACCESSKEY;
+const NCP_secretKey = process.env.SECRETKEY;
+
+// 네이버 Signiture API
+const date = Date.now().toString();
+const uri = NCP_serviceID;
+const secretKey = NCP_secretKey;
+const accessKey = NCP_accessKey;
+const method = 'POST';
+const space = ' ';
+const newLine = '\n';
+const url = `https://sens.apigw.ntruss.com/sms/v2/services/${uri}/messages`;
+const url2 = `/sms/v2/services/${uri}/messages`;
+
+const hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA256, secretKey);
+
+hmac.update(method);
+hmac.update(space);
+hmac.update(url2);
+hmac.update(newLine);
+hmac.update(date);
+hmac.update(newLine);
+hmac.update(accessKey);
+
+const hash = hmac.finalize();
+const signature = hash.toString(CryptoJS.enc.Base64);
+
+app.post('/', function (req, res) {
   const phoneNumber = req.body.phoneNumber;
 
   Cache.del(phoneNumber);
 
-  let verifyCode = Math.floor(Math.random() * 1000000) + 100000;
-  if (verifyCode > 1000000) {
-    verifyCode = verifyCode - 100000;
-  }
+  //인증번호 생성
+  const verifyCode = Math.floor(Math.random() * (999999 - 100000)) + 100000;
 
   Cache.put(phoneNumber, verifyCode.toString());
 
-  try {
-    client.messages
-      .create({
-        body: `[하트링크] 인증번호는 ${verifyCode} 입니다.`,
-        from: '+12246430642',
-        to: `+82${phoneNumber}`,
-      })
-      .then((message) => console.log(message.sid));
-  } catch (error) {
-    console.log(error);
-  }
-  return res.send('인증번호가 발송되었습니다.');
+  request(
+    {
+      method: method,
+      json: true,
+      uri: url,
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+        'x-ncp-iam-access-key': NCP_accessKey,
+        'x-ncp-apigw-timestamp': date,
+        'x-ncp-apigw-signature-v2': signature,
+      },
+      body: {
+        type: 'SMS',
+        contentType: 'COMM',
+        countryCode: '82',
+        from: '01032587579',
+        content: `[하트링크] 인증번호는 ${verifyCode} 입니다.`,
+        messages: [
+          {
+            to: `${phoneNumber}`,
+          },
+        ],
+      },
+    },
+    function (err, res, html) {
+      if (err) console.log(err);
+      else {
+        console.log(html);
+      }
+    },
+  );
+
+  res.json('인증번호가 발송되었습니다.');
 });
 
 app.post('/verify', (req, res) => {
